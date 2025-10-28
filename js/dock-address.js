e49a7b086584b8f47fd254b32aa376221a530389
Boo-Beard <81385452+Boo-Beard@users.noreply.github.com>
2025-10-28 10:57:52 +0400

    Update dock-address.js

--- js/dock-address.js
+++ js/dock-address.js
@@ -475,8 +475,8 @@ function renderDock(t, detectedChain) {
         <div class="stats-title">
           <i class="fas fa-chart-line"></i>
           ${t.name || "Unknown"}
-<span class="contract-address" id="contractAddress" style="font-size: 13px;padding: 10px;vertical-align: -2px;</span>
-<button class="copy-ca-btn" id="copyContract"><i class="fa-regular fa-copy"></i></button>
+          <span class="contract-address" id="contractAddress"></span>
+          <button class="copy-ca-btn" id="copyContract"><i class="fa-regular fa-copy"></i></button>
         </div>
       </div>
       <div class="chain-badge is-static" title="${chainInfo.name}">
