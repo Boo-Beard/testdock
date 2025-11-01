4632c8a117d470cca1670e14e694952049fe1944
Boo-Beard <81385452+Boo-Beard@users.noreply.github.com>
2025-11-01 10:58:29 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -29,7 +29,7 @@ export default {
     // Background mode: 'color' or 'video'
     type: 'color',
     // Solid color override (null uses theme --bg-dark)
-    color: "#31794b",
+    color: "#080a14",
     // Optional background video URL
     videoUrl: '',
     // Opacity applied to background media
