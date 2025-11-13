19f1bc36d110c17e67f8238e187647c860e166bc
Boo-Beard <81385452+Boo-Beard@users.noreply.github.com>
2025-11-13 12:13:13 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -45,7 +45,7 @@ export default {
     // Opacity applied to background media
     opacity: 1,
     // CSS filter applied to background media
-    filter: 'brightness(1) contrast(1)',
+     //filter: 'brightness(1) contrast(1)',
     // Overlay darkness for readability
     overlayOpacity: 1
   },
